f0195ad8bd897d06a44d5ebe469d5135522aca9b
Carlos Miranda <cmiranda@mobydigital.com>
2021-10-01 19:22:06 -0300

    trajimos archivos del framework

---
+++ Models/Student.php
@@ -0,0 +1,20 @@
+<?php
+    namespace Models;
+
+    use Models\Person as Person;
+
+    class Student extends Person
+    {
+        private $recordId;
+
+        public function getRecordId()
+        {
+            return $this->recordId;
+        }
+
+        public function setRecordId($recordId)
+        {
+            $this->recordId = $recordId;
+        }
+    }
+?>
