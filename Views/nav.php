f296b7e226814bfdc3a4391342197360977b39d0
Ezequiel <exeziel@hotmail.com>
2021-10-15 17:47:27 -0300

    Company Nav

--- Views/nav.php
+++ Views/nav.php
@@ -4,10 +4,10 @@
      </span>
      <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Views/student-add">Add Student</a>
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/Add">Add Student</a>
           </li>
           <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Views/student-list">List Students</a>
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowListView">List Students</a>
           </li>          
      </ul>
 </nav>
