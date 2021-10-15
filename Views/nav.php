e14489648fe9cec2f7ec12e2e683e3f8c2f1008f
Carlos Miranda <cmiranda@mobydigital.com>
2021-10-15 18:25:15 -0300

    corrigiendo errores del merge

--- Views/nav.php
+++ Views/nav.php
@@ -4,11 +4,7 @@
      </span>
      <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-<<<<<<< HEAD
                <a class="nav-link" href="<?php echo FRONT_ROOT ?>company/ListCompanies">List Companies</a>
-=======
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/Add">Add Student</a>
->>>>>>> 7a04de7bc6db23f5c3f22a98f0dc1a2164a647fc
           </li>
           <li class="nav-item">
                <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowListView">List Students</a>
