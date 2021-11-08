65904bdca03178c258ee68895532c738c61252e2
Suarez <danilomdq@gmail.com>
2021-11-08 19:13:17 -0300

    Complete funcion getAllPostulantesByJobOffer y cree funcion retriveData

--- DAO/PostulantesByJobOfferDAO.php
+++ DAO/PostulantesByJobOfferDAO.php
@@ -10,14 +10,47 @@ use \PDOException AS PDOException;
 
 class PostulantesByJobOfferDAO implements IPostulantesByJobOfferDAO
 {
-    private $studentList;    
+    private $postulantesByJobOfferList;  
     private $connection;
     
     
     public function __construct()
-    {
-        $this->jobOffersList = array();
-        $this->jobOfferByCompany = array();
-        $this->jobOfferByCareer = array();
+    {       
+        $this->postulantesByJobOfferList = array();       
     }
+
+    function getAllPostulantesByJobOffer($jobOfferId)
+    {
+        $sql = "SELECT * FROM postulante_por_jobo_offer WHERE jobOfferId=:jobOfferId AND active=:active";
+        $parameters['jobOfferId'] = $jobOfferId;
+        $parameters['active'] = true;
+
+        try {
+            $this->connection = Connection::getInstance();
+            $this->jobOffers= $this->connection->executeNonQuery($sql, $parameters);
+        } catch (\PDOException $exception) {
+            throw $exception;
+        }
+
+        if($this->postulantesByJobOfferList != null){
+            return $this->retrieveData();
+        }else{
+            return false;
+        }    }
+
+      
+    private function retrieveData()
+        {
+            $listToReturn = array();
+    
+            foreach ($this->postulantesByJobOfferList as $values) {
+                $postulantesByJobOffer = new PostulantesByJobOffer();
+                $postulantesByJobOffer->setStudentId($values['studentId']);
+                $postulantesByJobOffer->setJobOfferId($values['jobOfferId']);            
+                
+                array_push($listToReturn, $postulantesByJobOffer);
+            }
+            return  $listToReturn;
+        }   
+    
 }
